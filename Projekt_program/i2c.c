efb4892977dc84d1a142cd9269382fd76fd6bb2b
DarthSkipper <34075807+DarthSkipper@users.noreply.github.com>
2018-01-14 19:35:32 +0100

    Update i2c.c

--- Projekt_program/i2c.c
+++ Projekt_program/i2c.c
@@ -16,16 +16,22 @@
 *----------------------------------------------------------------------------*/
 void initI2C(void)
 {
-	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;	/* Enable clock for PORTC */
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK | SIM_SCGC5_PORTA_MASK;	/* Enable clock for PORTE */
 	SIM->SCGC4 |= SIM_SCGC4_I2C1_MASK;	/* Enable clock for I2C1 */
 	
 	PORTE->PCR[PORTE_SCL] = PORT_PCR_MUX(6);		/* PORTE 1 as I2C_SCL */
 	PORTE->PCR[PORTE_SDA] = PORT_PCR_MUX(6);		/* PORTE 0 as I2C_SDA */
 	
 	I2C1->F = 0x14;		/* SDA hold time = 2.125us, SCL start hold time = 4.25us, SCL stop hold time = 5.125us */	
-	I2C1->C1 |= I2C_C1_IICEN_MASK | I2C_C1_IICIE_MASK; /* I2C enable with interrupts */
-	
+	I2C1->C1 |= I2C_C1_IICEN_MASK | I2C_C1_IICIE_MASK; /* I2C enable with interrupts */	
 }
+/*----------------------------------------------------------------------------
+	Interrupt handler for I2C1
+*----------------------------------------------------------------------------*/
+//void I2C1_IRQHandler(void)
+//{
+//	
+//}
 /*----------------------------------------------------------------------------
 	Enable ACK
 *----------------------------------------------------------------------------*/
@@ -92,20 +98,39 @@ void I2C_wait(void)
 	I2C1->S |= I2C_S_IICIF_MASK; 		/* Clear the interrupt flag */   
 }
 /*----------------------------------------------------------------------------
-	Write 8 bits of data to device register 
+	Write one byte of data to device register 
 *----------------------------------------------------------------------------*/
 void I2C_writeRegister(uint8_t u8SlaveAddress, uint8_t u8RegisterAddress, uint8_t u8Data)
 {
 	I2C_start();	          
-	I2C_writeByte(u8SlaveAddress & 0xFE);									/* Send I2C device address with W/R bit = 0 */
+	I2C_writeByte(u8SlaveAddress & 0xFE);				/* Send I2C device address with W/R bit = 0 */
 	I2C_wait();
 
-	I2C_writeByte(u8RegisterAddress);										/* Send register address */
+	I2C_writeByte(u8RegisterAddress);						/* Send register address */
 	I2C_wait();
 
-	I2C_writeByte(u8Data);												/* Send the data */
+	I2C_writeByte(u8Data);											/* Send the data */
 	I2C_wait();
 
+	I2C_stop();
+
+  delay_mc(50);
+}
+/*----------------------------------------------------------------------------
+	Write byte's of data to device register TSL only!!!!
+*----------------------------------------------------------------------------*/
+void I2C_writeRegister_TSL(uint8_t u8SlaveAddress, uint8_t u8RegisterAddress, uint8_t u8Data)
+{
+	I2C_start();	          
+	I2C_writeByte(u8SlaveAddress & 0xFE);				/* Send I2C device address with W/R bit = 0 */
+	I2C_wait();
+
+	I2C_writeByte(0x80 | u8RegisterAddress);						/* Send register address */
+	I2C_wait();
+
+	I2C_writeByte(u8Data);											/* Send the data */
+	I2C_wait();
+	
 	I2C_stop();
 
   delay_mc(50);
@@ -118,15 +143,15 @@ uint8_t I2C_ReadRegister(uint8_t u8SlaveAddress, uint8_t u8RegisterAddress)
 	uint8_t result;
 	  
 	I2C_start();	          
-	I2C_writeByte(u8SlaveAddress & 0xFE);							/* Send I2C device address with W/R bit = 0 */
+	I2C_writeByte(u8SlaveAddress & 0xFE);				/* Send I2C device address with W/R bit = 0 */
 	I2C_wait();										
 	
-	I2C_writeByte(u8RegisterAddress);								/* Send register address */
+	I2C_writeByte(u8RegisterAddress);						/* Send register address */
 	I2C_wait();
 
 	I2C_repeatedStart();
 	
-	I2C_writeByte(u8SlaveAddress | 0x01);		/* Send I2C device address this time with W/R bit = 1 */ 
+	I2C_writeByte(u8SlaveAddress | 0x01);				/* Send I2C device address this time with W/R bit = 1 */ 
 	I2C_wait();	
 
 	I2C_enterRxMode();
@@ -147,10 +172,10 @@ void I2C_ReadMultiRegisters(uint8_t u8SlaveAddress, uint8_t u8RegisterAddress, u
 	char i;
 	
 	I2C_start();	
-	I2C_writeByte(u8SlaveAddress & 0xFE);										/* Send I2C device address with W/R bit = 0 */
+	I2C_writeByte(u8SlaveAddress & 0xFE);							/* Send I2C device address with W/R bit = 0 */
 	I2C_wait();	
 	
-	I2C_writeByte(u8RegisterAddress);										/* Send register address */
+	I2C_writeByte(u8RegisterAddress);									/* Send register address */
 	I2C_wait();
 
 	I2C_repeatedStart();
