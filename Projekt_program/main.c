a7f0676142c9ca9586b42af52b55374508c36541
DarthSkipper <34075807+DarthSkipper@users.noreply.github.com>
2018-01-14 20:00:09 +0100

    Update main.c

--- Projekt_program/main.c
+++ Projekt_program/main.c
@@ -19,46 +19,37 @@ int main(void)
 {
 	initI2C();
 	slcdInitialize();
-	ledsInitialize();
+	//ledsInitialize();
 	turnOnSensors();
-	//rgbON();
 	uint8_t next = 1;
 	while(1) 
 	{
-//		switch(next)
-//		{
-//			case 1:
-//				slcdDisplay(temperatureRead()*100, 10);
-//				dot(2);
-//				slcdSet(0x0C, 4);
-//				next++;
-//				FPTC->PCOR = (1UL<<8);
-//			delay_mc(500);
-//				break;
-//			case 2:
-//				slcdDisplay(pressureRead(), 10);
-//				next++;
-//				FPTB->PCOR = (1UL<<18);
-//			  FPTD->PCOR = (1UL<<3);
-//				delay_mc(500);
-//				break;
-//			case 3:
-//				slcdDisplay(humidityRead(), 10);
-//				dot(3);
-//				next++;
-//				FPTD->PCOR = (1UL<<3);
-//				delay_mc(500);
-//				break;
-//			case 4:
-				slcdDisplay(lightRead(), 10);
-//				dot(3);
-//				next=1;
-//				FPTD->PCOR = (1UL<<3);
-//				FPTC->PCOR = (1UL<<8);
-//				delay_mc(500);
-//				break;
-//		}
-//		rgbOFF();
+		switch(next)
+		{
+			case 1:
+				slcdDisplay(temperatureRead()*100, 10);
+				dot(2);
+				slcdSet(0x0C, 4);
+				next++;
+			delay_mc(500);
+				break;
+			case 2:
+				slcdDisplay(pressureRead(), 10);
+				next++;
+				delay_mc(500);
+				break;
+			case 3:
+			slcdDisplay(humidityRead(), 10);
+				next++;
+			delay_mc(500);
+				break;
+			case 4:
+				slcdDisplay(lightRead()*10, 10);
+				dot(3);
+				next=1;
+				delay_mc(500);
+				break;
+		}
 	};
 
 }
